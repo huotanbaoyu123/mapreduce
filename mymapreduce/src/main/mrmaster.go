f9f8454a82013fbea5eea95f2de10094f2eedb7c
huotanbaoyu123 <mb2geyu@163.com>
2021-11-12 11:03:03 +0800

    add all text
    1.add all text
    2.now we want to write linux code

--- mymapreduce/src/main/mrmaster.go
+++ mymapreduce/src/main/mrmaster.go
@@ -12,12 +12,12 @@ func main() {
 	}
 
 
-	//files:=[] string{"src/main/pg-being_ernest.txt","src/main/pg-dorian_gray.txt",
-	//	"src/main/pg-frankenstein.txt","src/main/pg-grimm.txt",
-	//	"src/main/pg-huckleberry_finn.txt","src/main/pg-metamorphosis.txt",
-	//	"src/main/pg-sherlock_holmes.txt","src/main/pg-tom_sawyer.txt"}
+	files:=[] string{"src/main/pg-being_ernest.txt","src/main/pg-dorian_gray.txt",
+		"src/main/pg-frankenstein.txt","src/main/pg-grimm.txt",
+		"src/main/pg-huckleberry_finn.txt","src/main/pg-metamorphosis.txt",
+		"src/main/pg-sherlock_holmes.txt","src/main/pg-tom_sawyer.txt"}
 //	m := mr.MakeMaster(os.Args[1:], 10)
-	files:=[] string{"src/main/pg-being_ernest.txt"}
+	//files:=[] string{"src/main/pg-being_ernest.txt"}
 	//m := mr.MakeMaster(files, len(files))
 	m := mr.MakeCoordinator(files, len(files))
 	for m.Done() == false {
