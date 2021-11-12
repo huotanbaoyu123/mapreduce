b63229a9d730c644a39f73a944553da2b11e745f
huotanbaoyu123 <mb2geyu@163.com>
2021-11-12 18:28:47 +0800

    remove rpc、global no used files
    1.we romve some noUsed

--- mymapreduce/src/mr/Myrpc.go
+++ mymapreduce/src/mr/Myrpc.go
@@ -1,5 +1,9 @@
 package mr
 
+import (
+	"os"
+	"strconv"
+)
 
 type Tasktype int
 const (
@@ -60,4 +64,49 @@ type FinishedTaskArgs struct{
 
 type FinishedTaskReply struct{
 
+}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the master.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func masterSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+
+	if IsLinux!=true{
+		s =":2234"
+	}
+
+	return s
+}
+
+func masterNetwork() string {
+	s :="unix"
+	if IsLinux!=true{
+		s ="tcp"
+	}
+	return s
+}
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
+
+type TryMapArgs struct {
+}
+
+type TryMapReply struct {
+	// if should not run map, run reduce
+	RunMap bool
 }
--- mymapreduce/src/mr/globalVar.go
+++
@@ -1,5 +0,0 @@
-package mr
-//
-//const (
-//	isLinux = false
-//)
